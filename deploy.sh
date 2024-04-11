1d1fbd01b60b0d30a535b7a7bc44aa0c85b0749f
Fawas <fawasrichard@gmail.com>
2024-04-11 22:01:39 +0100

    10 commit

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,7 @@ pm2 stop NotesSaverApp
 cd NotesSaverApp/
 # Install application dependencies
 npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 # Start the application with the process name example_app using pm2 .
 pm2 start ./bin/www --name NotesSaverApp
