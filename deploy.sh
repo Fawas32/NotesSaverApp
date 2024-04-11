dfa1bce22a8d26db00b529fd29a693382da37270
Fawas <fawasrichard@gmail.com>
2024-04-11 20:28:56 +0100

    5 commit

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer
 sudo npm install -g pm2
 #stop any instance of our application running currently
-pm2 stop simple_application
+pm2 stop SaveNotesApp
 #change directory into folder where application is downloaded
-cd SimpleApplication/
+cd SaveNotesApp/
 # Install application dependencies
 npm install
 # Start the application with the process name example_app using pm2 .
